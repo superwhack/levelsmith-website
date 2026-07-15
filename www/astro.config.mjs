84752058888f394a74867a28293953bf922269b3
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2026-07-15 15:59:45 -0400

    began styling homepage

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    fonts: [{
+    provider: fontProviders.local(),
+    name: "IBM Plex Mono",
+    cssVariable: "--font-ibm-plex-mono",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/IBMPlexMono-Regular.ttf'],
+        weight: 'normal',
+        style: 'normal'
+      }]
+    }
+  }],
   vite: {
     plugins: [tailwindcss()],
   },
